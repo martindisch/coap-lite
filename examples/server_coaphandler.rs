807b710e5866b09b4f685896e970386b5e8f1f71
chrysn <chrysn@fsfe.org>
2021-08-10 16:11:09 +0200

    examples: Remove complexity
    There is a dedicated set of examples out now; no need to pull in
    complexity to this example, which mainly shows that coap_message & co
    can be used together with this crate.

--- examples/server_coaphandler.rs
+++ examples/server_coaphandler.rs
@@ -1,52 +1,29 @@
-// Note that this example requires nightly for coap-handler's #![feature(iter_order_by)]
+/// This is a very simple example using the coap_message/_handler abstractions.
+///
+/// For an example that shows more advanced resources, see
+/// <https://gitlab.com/chrysn/coap-message-demos>, which has a coaplite example of its own, and
+/// features a much larger variety of interactive resources.
+///
+/// Note that this example requires nightly due to coap_handler's requirements.
 
 use coap_lite::{CoapRequest, Packet};
 use std::net::UdpSocket;
 
-use serde::{Deserialize, Serialize};
-
 use coap_handler::implementations::{
-    HandlerBuilder, SimpleCBORHandler, SimpleCBORWrapper, SimpleRenderable,
-    SimpleRendered, TypedStaticResponse,
+    HandlerBuilder, SimpleRendered, TypedStaticResponse,
 };
 use coap_handler::Handler as _;
 
-use coap_numbers;
-
 fn main() {
-    let mut value_at_cbor = StoredCBOR::new();
-
-    struct Time;
-
-    impl SimpleRenderable for Time {
-        fn render<W: core::fmt::Write>(&mut self, writer: &mut W) {
-            write!(
-                writer,
-                "It's {} seconds past epoch.",
-                std::time::SystemTime::now()
-                    .duration_since(std::time::SystemTime::UNIX_EPOCH)
-                    .unwrap()
-                    .as_secs()
-            )
-            .unwrap();
-        }
-
-        fn content_format(&self) -> Option<u16> {
-            Some(0 /* text/plain */)
-        }
-    }
-
     let mut handler = coap_handler::implementations::new_dispatcher()
         .at(
             &[".well-known", "core"],
             TypedStaticResponse {
-                payload: b"</>,</time>,</cbor>",
+                payload: b"</>,</time>",
                 contentformat: &[40],
             },
         )
-        .at(&[], SimpleRendered("Welcome to the Demo server"))
-        .at(&["time"], SimpleRendered(Time))
-        .at(&["cbor"], SimpleCBORWrapper::new(&mut value_at_cbor));
+        .at(&[], SimpleRendered("Welcome to the Demo server"));
 
     let socket = UdpSocket::bind("127.0.0.1:5683").unwrap();
     let mut buf = [0; 1280];
@@ -67,52 +44,5 @@ fn main() {
         socket
             .send_to(&packet[..], &src)
             .expect("Could not send the data");
-    }
-}
-
-// Just a more complex item for the list of resources
-
-#[derive(Serialize, Deserialize, Clone)]
-struct StoredCBOR {
-    hidden: bool,
-    number: usize,
-    label: String,
-    list: Vec<usize>,
-}
-
-impl StoredCBOR {
-    fn new() -> Self {
-        Self {
-            hidden: false,
-            number: 32,
-            label: "Hello".to_string(),
-            list: vec![1, 2, 3],
-        }
-    }
-}
-
-impl SimpleCBORHandler for &mut StoredCBOR {
-    type Get = StoredCBOR;
-    type Put = StoredCBOR;
-    type Post = StoredCBOR;
-
-    fn get(&mut self) -> Result<StoredCBOR, u8> {
-        if self.hidden {
-            return Err(coap_numbers::code::FORBIDDEN);
-        }
-        Ok(self.clone())
-    }
-
-    fn put(&mut self, new: &StoredCBOR) -> u8 {
-        if new.label.contains("<") {
-            // No HTML injection please ;-)
-            return coap_numbers::code::BAD_REQUEST;
-        }
-        **self = new.clone();
-        coap_numbers::code::CHANGED
-    }
-
-    fn post(&mut self, _: &StoredCBOR) -> u8 {
-        coap_numbers::code::METHOD_NOT_ALLOWED
     }
 }
